923d9f03687f82c1b9323338c3360df9888a0b88
Dario Marcelino <dario@appscot.com>
2014-11-24 20:09:13 +0000

    Assocations, 2 new helper methods: reduceNestedObjects and forEachNested. This are to be used in expanding the RIDs returned by fetchPlan

--- test/unit/utils.test.js
+++ test/unit/utils.test.js
@@ -40,4 +40,103 @@ describe('utils helper class', function () {
 		
 		done();
 	});
+	
+	
+	it('reduceNestedObjects: should properly traverse nested objects and reduce them', function (done) {
+	  var callback = function(accumulator, obj, key){
+	    if(!obj.id)
+	      return accumulator;
+	      
+	    if(!accumulator[obj.id])
+	      accumulator[obj.id] = 1;
+	    else
+        accumulator[obj.id]++;
+        
+      return accumulator;
+	  };
+	  
+	  var collection1 = { id: '#13:1' };
+	  var result = utils.reduceNestedObjects(collection1, callback, {});
+	  assert.equal(Object.keys(result).length, 1);
+	  assert.equal(result['#13:1'], 1);
+	  
+	  
+	  var collection2 = { id: '#13:1', name: 'John Dow', child: { id: '#13:2'} };
+	  var result = utils.reduceNestedObjects(collection2, callback, {});
+    assert.equal(Object.keys(result).length, 2);
+    assert.equal(result['#13:2'], 1);
+    
+    
+    var collection3 = { id: '#13:1', name: 'John Dow', child: { id: '#13:2'}, noId: {name: 'Miles'} };
+    var result = utils.reduceNestedObjects(collection3, callback, {});
+    assert.equal(Object.keys(result).length, 2);
+    
+    
+    var collection4 = { id: '#13:1', name: 'John Dow', children: [{ id: '#13:2'}, { id: '#13:3'}]};
+    var result = utils.reduceNestedObjects(collection4, callback, {});
+    assert.equal(Object.keys(result).length, 3);
+    assert.equal(result['#13:3'], 1);
+    
+    
+    var collection5 = { id: '#13:1', name: 'John Dow', children: [{ id: '#13:2'}, { id: '#13:3', parent: { id: '#13:1' }}]};
+    var result = utils.reduceNestedObjects(collection5, callback, {});
+    assert.equal(Object.keys(result).length, 3);
+    assert.equal(result['#13:3'], 1);
+    assert.equal(result['#13:2'], 1);
+    assert.equal(result['#13:1'], 2);
+	  
+	  done();
+	});
+	
+	
+  it('forEachNested: should properly traverse nested properties applying callback', function (done) {
+    var result = {};
+    var callback = function(value, key, parent){
+      result[value] = key;
+    };
+    
+    var collection1 = { id: '#13:1' };
+    utils.forEachNested(collection1, callback);
+    assert.equal(Object.keys(result).length, 1);
+    assert.equal(result['#13:1'], 'id');
+    
+    
+    var collection2 = { id: '#13:1', name: 'John Dow', child: { id: '#13:2'} };
+    utils.forEachNested(collection2, callback);
+    assert.equal(Object.keys(result).length, 3);
+    assert.equal(result['#13:2'], 'id');
+    assert.equal(result['John Dow'], 'name');
+    
+    
+    var collection3 = { id: '#13:1', name: 'John Dow', child: { id: '#13:2'}, noId: {name: 'Miles'} };
+    utils.forEachNested(collection3, callback);
+    assert.equal(Object.keys(result).length, 4);
+    assert.equal(result['Miles'], 'name');
+    
+    
+    var collection4 = { id: '#13:1', name: 'John Dow', children: [{ id: '#13:2'}, { id: '#13:3'}]};
+    utils.forEachNested(collection4, callback);
+    assert.equal(Object.keys(result).length, 5);
+    assert.equal(result['#13:3'], 'id');
+    
+    
+    var collection5 = { id: '#13:1', name: 'John Dow', children: [{ id: '#13:2'}, { id: '#13:3', parent: { id: '#13:1' }}]};
+    utils.forEachNested(collection5, callback);
+    assert.equal(Object.keys(result).length, 5);
+    
+    var modifyCollection5Callback = function(value, key, parent){
+      if(key === 'id')
+        parent[key] = value + '_new';
+    };
+    utils.forEachNested(collection5, modifyCollection5Callback);
+    assert.equal(Object.keys(collection5).length, 3);
+    assert.equal(collection5.id, '#13:1_new');
+    assert.equal(collection5.children[0].id, '#13:2_new');
+    assert.equal(collection5.children[1].id, '#13:3_new');
+    assert.equal(collection5.children[1].parent.id, '#13:1_new');
+    
+    
+    done();
+  });
+	
 });
