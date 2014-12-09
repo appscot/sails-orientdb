25bd2bdd61c4b7f69c6a5d4debcac0d3e81bc613
Dario Marcelino <dario@appscot.com>
2014-12-09 21:08:38 +0000

    Associations cleanAttributes for one-to-one associations

--- test/integration-orientdb/tests/associations/manyThrough.find.js
+++ test/integration-orientdb/tests/associations/manyThrough.find.js
@@ -12,7 +12,7 @@ describe('Association Interface', function() {
     var stadiumRecord, teamRecord, ownerRecord;
 
     before(function(done) {
-      Associations.Stadium.create({ name: 'populate stadium'}, function(err, stadium) {
+      Associations.Stadium.create({ name: 'populate stadium', sponsor: {name: 'someBrand'}}, function(err, stadium) {
         if(err) return done(err);
         stadiumRecord = stadium;
           Associations.Team.create({ name: 'populate team', mascot: 'elephant' }, function(err, team) {
@@ -42,7 +42,7 @@ describe('Association Interface', function() {
     ////////////////////////////////////////////////////
     describe('without populate', function() {
       
-      it('should not have team or friend records, nor edges', function(done) {
+      it('findOne: should not have team or friend records, nor edges', function(done) {
         Associations.Stadium.findOne(stadiumRecord.id)
           .then(function(stadium){
             assert(stadium.teams.length === 0);
@@ -50,6 +50,23 @@ describe('Association Interface', function() {
             assert(!stadium.out_venueTable);
             assert(!stadium.in_ownsTable);
             assert(!stadium['@type']);
+            assert(!stadium.sponsor['@type']);
+            done();
+          })
+          .catch(function(err){
+            done(err);
+          });
+      });
+      
+      it('should not have team or friend records, nor edges', function(done) {
+        Associations.Stadium.find(stadiumRecord.id)
+          .then(function(stadiums){
+            assert(stadiums[0].teams.length === 0);
+            assert(stadiums[0].owners.length === 0);
+            assert(!stadiums[0].out_venueTable);
+            assert(!stadiums[0].in_ownsTable);
+            assert(!stadiums[0]['@type']);
+            assert(!stadiums[0].sponsor['@type']);
             done();
           })
           .catch(function(err){
@@ -60,9 +77,10 @@ describe('Association Interface', function() {
 
     describe('with populate', function() {
       
-      it('should not have friend records, nor edges', function(done) {
+      it('findOne: should not have friend records, nor edges', function(done) {
         Associations.Stadium.findOne(stadiumRecord.id)
           .populate('teams')
+          .populate('sponsor')
           .then(function(stadium){
             assert(stadium.teams.length === 1);
             assert(stadium.owners.length === 0);
@@ -70,6 +88,42 @@ describe('Association Interface', function() {
             assert(!stadium.in_ownsTable);
             assert(!stadium.teams[0].in_venueTable);
             assert(!stadium['@type']);
+            assert(stadium.sponsor);
+            assert(!stadium.sponsor['@type']);
+            done();
+          })
+          .catch(function(err){
+            done(err);
+          });
+      });
+      
+      it('should not have friend records, nor edges', function(done) {
+        Associations.Stadium.find(stadiumRecord.id)
+          .populate('teams')
+          .populate('sponsor')
+          .then(function(stadiums){
+            assert(stadiums[0].teams.length === 1);
+            assert(stadiums[0].owners.length === 0);
+            assert(!stadiums[0].out_venueTable);
+            assert(!stadiums[0].in_ownsTable);
+            assert(!stadiums[0].teams[0].in_venueTable);
+            assert(!stadiums[0]['@type']);
+            assert(stadiums[0].sponsor);
+            assert(!stadiums[0].sponsor['@type']);
+            done();
+          })
+          .catch(function(err){
+            done(err);
+          });
+      });
+      
+      it('findOne one-to-one: should not have team or friend records, nor edges', function(done) {
+        Associations.Stadium.findOne(stadiumRecord.id)
+          .populate('sponsor')
+          .then(function(stadium){
+            assert(!stadium['@type']);
+            assert(stadium.sponsor);
+            assert(!stadium.sponsor['@type']);
             done();
           })
           .catch(function(err){
