a3a2037283cec9696c5fa3ca345824cd57c8caff
Dario Marcelino <dario@appscot.com>
2015-02-15 19:43:46 +0000

    Added test to deleteEdge with @class set

--- test/integration-orientdb/tests/adapterCustomMethods/deleteEdges.js
+++ test/integration-orientdb/tests/adapterCustomMethods/deleteEdges.js
@@ -27,7 +27,13 @@ describe('Adapter Custom Methods', function() {
             
             assert(edge);
             
-            done();
+            Associations.User_resource.createEdge(user.id, profile.id, { '@class': 'followsTable' }, function(err, edge){
+              if(err) return done(err);
+              
+              assert(edge);
+              
+              done();
+            });
           });
           
         });
@@ -40,6 +46,24 @@ describe('Adapter Custom Methods', function() {
       /////////////////////////////////////////////////////
       // TEST METHODS
       ////////////////////////////////////////////////////
+      
+      it('should delete an edge of class followsTable', function(done) {
+        Associations.User_resource.deleteEdges(user.id, profile.id, { '@class': 'followsTable' }, function(err, count){
+          if(err) return done(err);
+          
+          Associations.User_resource.getDB(function(db){
+            db
+              .query('select expand(outE()) from ' + user.id)
+              .then(function(resultSet){
+                assert(_.isArray(resultSet), 'is array');
+                assert.equal(resultSet.length, 1);
+                assert.notEqual(resultSet[0]['@class'], 'followsTable');
+                done();
+              })
+              .catch(done);
+          });
+        });
+      });
       
       
       it('should delete an edge between a user and profile', function(done) {
@@ -54,7 +78,7 @@ describe('Adapter Custom Methods', function() {
                 assert.equal(resultSet.length, 0);
                 done();
               })
-              .catch(function(err) { done(err); } );
+              .catch(done);
           });
         });
       });
